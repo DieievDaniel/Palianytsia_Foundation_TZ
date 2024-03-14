29e5f2e1c13e844d247bd74a4ecda7e7d65ad5b3
DieievDaniel <120338641+DieievDaniel@users.noreply.github.com>
2024-03-15 00:31:19 +0200

    World and movement
    I have created the enviroment and player. Also, I have created movement logic for our player

---
+++ PalianytsaFoudationTZ/Assets/Scripts/InputManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameInput gameInput;
+    private Vector2 moveInput;
+    private bool jumpInput;
+    private bool isGrounded;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameInput = new GameInput();
+    }
+
+    private void OnEnable()
+    {
+        gameInput.Enable();
+        gameInput.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        gameInput.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+        gameInput.Player.Jump.performed += ctx => jumpInput = true;
+    }
+
+    private void OnDisable()
+    {
+        gameInput.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (jumpInput)
+        {
+            Jump();
+            jumpInput = false; // Ñבנמס פכאדא ןמסכו ןנûזךא
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + transform.TransformDirection(movement));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsGroundedUpdate(collision, true);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        IsGroundedUpdate(collision, false);
+    }
+
+    private void IsGroundedUpdate(Collision collision, bool value)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = value;
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
